a2f9fc86eae1ac85f3e3a35741619671b2ba41a6
gaogao-9 <gaotuki@gmail.com>
2020-01-07 06:11:01 +0900

    fix: 昔ながらの100%指定はだめでした

--- components/Template/DefaultTemplate.tsx
+++ components/Template/DefaultTemplate.tsx
@@ -5,22 +5,40 @@ type Props = {
   children: React.ReactNode;
 };
 
-const DefaultTemplate: React.FC<Props> = ({ children }) => (
-  <>
-    <Head>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-      />
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://fonts.googleapis.com/css?family=Noto+Serif+JP:200&display=swap"
-      />
-      <link rel="stylesheet" type="text/css" href="/global.css" />
-    </Head>
-    {children}
-  </>
-);
+const DefaultTemplate: React.FC<Props> = ({ children }) => {
+  React.useEffect(() => {
+    const onResize = (): void => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    onResize();
+
+    window.addEventListener("resize", onResize, { passive: true });
+
+    return (): void => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://fonts.googleapis.com/css?family=Noto+Serif+JP:200&display=swap"
+        />
+        <link rel="stylesheet" type="text/css" href="/global.css" />
+      </Head>
+      {children}
+    </>
+  );
+};
+
 export default DefaultTemplate;
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,8 @@ const Background = styled.div`
   align-items: center;
   overflow: hidden;
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
   background-color: #000000;
 
   ${media({ orientation: "portrait" })} {
