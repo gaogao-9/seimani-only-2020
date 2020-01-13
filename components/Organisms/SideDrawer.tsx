3a70d8ccb414fa6de6f5c35a902740d0bf671cef
gaogao-9 <gaotuki@gmail.com>
2020-01-13 11:26:37 +0900

    不要なimportの削除

--- components/Organisms/SideDrawer.tsx
+++ components/Organisms/SideDrawer.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styled from "@emotion/styled";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
