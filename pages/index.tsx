e4509ceef4f22019dba829b23b8d751a256a6edd
gaogao-9 <gaotuki@gmail.com>
2020-02-29 21:35:03 +0900

    トップページアニメーションの一時的な無効化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,14 @@
 import React from "react";
 import { useRouter } from "next/router";
-import Template from "@/components/Template/DefaultTemplate";
-import OpeningAnimBackground from "@/components/Atoms/OpeningAnimBackground";
-import OpeningAnimCharacterCutin from "@/components/Organisms/OpeningAnimCharacterCutin";
-import OpeningAnimPresentsLogo from "@/components/Organisms/OpeningAnimPresentsLogo";
-import {
-  OpeningScenes,
-  OpeningSceneContext,
-  useOpeningSceneContext,
-} from "@/hooks/contexts/OpeningSceneContext";
 
 const Page: React.FC = () => {
   const router = useRouter();
-  const openingSceneContextValue = useOpeningSceneContext(
-    OpeningScenes.CharacterCutin,
-  );
-  const EmptyElement: JSX.Element = React.useMemo(() => <></>, []);
-
-  const SceneElement = ((): JSX.Element => {
-    switch (openingSceneContextValue.scene) {
-      case OpeningScenes.CharacterCutin:
-        return <OpeningAnimCharacterCutin />;
-      case OpeningScenes.PresentsLogo:
-        return <OpeningAnimPresentsLogo />;
-      default:
-        return EmptyElement;
-    }
-  })();
 
   React.useEffect(() => {
-    if (openingSceneContextValue.scene === OpeningScenes.Finish) {
-      router.push("/top");
-    }
-  }, [openingSceneContextValue.scene]);
+    router.push("/top");
+  }, []);
 
-  return (
-    <Template>
-      <OpeningAnimBackground>
-        <OpeningSceneContext.Provider value={openingSceneContextValue}>
-          {SceneElement}
-        </OpeningSceneContext.Provider>
-      </OpeningAnimBackground>
-    </Template>
-  );
+  return <></>;
 };
 
 export default Page;
