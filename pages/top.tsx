a8169b5afb2442018f5042a94af73923fb8cee03
gaogao-9 <gaotuki@gmail.com>
2020-02-29 23:20:59 +0900

    ホバーアクション追加

--- pages/top.tsx
+++ pages/top.tsx
@@ -14,6 +14,7 @@ const StyledImage = styled.img`
 `;
 
 const Page: React.FC = () => {
+  const [topImageSrc, setTopImageSrc] = React.useState("/assets/img/top.jpg");
   const [twitterScriptSrc, setTwitterScriptSrc] = React.useState("");
   React.useEffect(() => {
     setTwitterScriptSrc("https://platform.twitter.com/widgets.js");
@@ -25,17 +26,37 @@ const Page: React.FC = () => {
     <></>
   );
 
+  const onImageEnter = React.useCallback(() => {
+    setTopImageSrc("/assets/img/top2.jpg");
+  }, []);
+
+  const onImageLeave = React.useCallback(() => {
+    setTopImageSrc("/assets/img/top.jpg");
+  }, []);
+
   return (
     <>
       <Head>{TwitterScriptElement}</Head>
       <Template>
         <Wrapper>
-          <Grid container justify="center" alignItems="center">
-            <Grid item xs={7}>
-              <StyledImage src="/assets/img/top.jpg" />
+          <Grid container justify="space-around" alignItems="center">
+            <Grid item xs={11} lg={8} xl={9}>
+              <StyledImage
+                src={topImageSrc}
+                onPointerEnter={onImageEnter}
+                onPointerLeave={onImageLeave}
+              />
             </Grid>
-            <Grid item xs={4}>
-              <div>
+            <Grid
+              container
+              item
+              xs={9}
+              lg={3}
+              xl={2}
+              justify="center"
+              alignItems="center"
+            >
+              <Grid container item xs={12} justify="center">
                 <a
                   data-size="large"
                   href="https://twitter.com/intent/tweet?text=%E6%94%BF%E5%89%A3%E3%83%9E%E3%83%8B%E3%83%95%E3%82%A7%E3%82%B9%E3%83%86%E3%82%A3%E3%82%A2%E3%82%AA%E3%83%B3%E3%83%AA%E3%83%BC%E5%90%8C%E4%BA%BA%E8%AA%8C%E5%8D%B3%E5%A3%B2%E4%BC%9A%20%E7%B7%8A%E6%80%A5%E4%BA%A4%E6%B5%81%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88&hashtags=%E6%94%BF%E5%89%A3%E3%83%9E%E3%83%8B%E3%83%95%E3%82%A7%E3%82%B9%E3%83%86%E3%82%A3%E3%82%A2,%E3%82%A2%E3%82%A4%E3%83%A0%E3%82%BD%E3%82%A6%E3%83%AA%E3%83%BC&url=https%3A%2F%2Ffestia.moe%2F&ref_src=twsrc%5Etfw"
@@ -44,8 +65,8 @@ const Page: React.FC = () => {
                 >
                   Tweet
                 </a>
-              </div>
-              <div>
+              </Grid>
+              <Grid container item xs={10} lg={12} justify="center">
                 <a
                   className="twitter-timeline"
                   data-height="500"
@@ -53,7 +74,7 @@ const Page: React.FC = () => {
                 >
                   Tweets by seimani_only
                 </a>
-              </div>
+              </Grid>
             </Grid>
           </Grid>
           <StarParticles />
