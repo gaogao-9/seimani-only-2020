ba44b07093da2144ad652edb6bd7f660461ee124
gaogao-9 <gaotuki@gmail.com>
2020-05-02 00:32:41 +0900

    feat: OGP関連を修正

--- components/Template/DefaultTemplate.tsx
+++ components/Template/DefaultTemplate.tsx
@@ -10,7 +10,8 @@ import Title from "@/components/Atoms/Title";
 const DefaultTemplate: React.FC = ({ children }) => {
   const router = useRouter();
   const route = routes.find(x => x.pathname === router.pathname);
-  const title = "政剣マニフェスティアオンリー同人誌即売会";
+  const title =
+    "政剣マニフェスティアオンリー同人誌即売会 緊急交流イベント 漕ぎ出せ！ソクバイ海Ⅲ";
   const subTitle = route?.pathname === "/top" ? "" : route?.title ?? "";
   const fullTitle = subTitle ? `${title} | ${subTitle}` : title;
   const titleElement = React.useMemo(() => Title({ title: fullTitle }), [
